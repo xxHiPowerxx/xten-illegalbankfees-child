f392baa96c54c6aa097e43b6d7cfd20f0e6af7fc
Ricky Adams <ra@main.local>
2020-03-25 17:08:16 -0700

    added function that scrolls body to target on hashchange or anchor click

--- dev/js/child-theme.js
+++ dev/js/child-theme.js
@@ -9,8 +9,38 @@
 					.prop("required", true);
 			});
 		}
+		function scrollToTarget() {
+			if ($("body").hasClass("xten-mobile-menu-active")) {
+				$("#sidebar-modal").modal("show");
+			} else {
+				var sideBarTop = $("#right-sidebar").position().top,
+					siteHeaderHeight = window.siteHeaderHeight
+						? parseFloat(window.siteHeaderHeight)
+						: $(".site-header")[0].getBoundingClientRect().height,
+					padding = 30,
+					scrollPosition = sideBarTop - siteHeaderHeight - padding;
+				$("html, body").animate({ scrollTop: scrollPosition });
+			}
+		}
+		function interceptHashChange() {
+			$(window).on("load hashchange", function(e) {
+				if (window.location.hash === "#contact-us") {
+					scrollToTarget();
+				}
+			});
+		}
+		function scrollToTargetOnClick() {
+			$('[href*="#contact-us"]').on("click", function() {
+				scrollToTarget();
+			});
+		}
+		function initScrollToTarget() {
+			interceptHashChange();
+			scrollToTargetOnClick();
+		}
 		function readyFuncs() {
 			addHTMLValidationToCF();
+			initScrollToTarget();
 		}
 		readyFuncs();
 	});
