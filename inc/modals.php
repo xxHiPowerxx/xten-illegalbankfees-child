5e7a427a3bf7f1de1b027370784104cfa40a163a
Ricky Adams <ra@main.local>
2020-05-19 13:31:33 -0700

    Now look for ${financial-institution} instead of ${title}

--- inc/modals.php
+++ inc/modals.php
@@ -19,7 +19,6 @@
 </div><!-- /#sidebar-modal -->
 
 <?php 
-$title    = get_the_title();
 $category_title;
 $category = get_the_category();
 if ( isset( $category ) ) :
@@ -28,20 +27,29 @@ if ( isset( $category ) ) :
 										null;
 endif;
 
+$word_to_find = '\${financial-institution}';
+$wrap_before = '<span data-input-id="input-institution" class="dynamicInputField">';
+$wrap_after  = '</span>';
 // Modal that displays if User Qualifies.
 $qualifying_popup_content = get_field( 'qualifying_popup_content_override', false, false );
 $qualifying_popup_content = $qualifying_popup_content ?
 														wp_kses_post( $qualifying_popup_content ) :
 														wp_kses_post( get_field( 'global_qualifying_popup_content', 'option', false ) );
 if ( $qualifying_popup_content ) :
-	$qualifying_popup_content = str_replace('${title}', $title, $qualifying_popup_content );
+	// Find ${financial-institution} and wrap in span.
+	$qualifying_popup_content = xten_word_wrap( 
+																$qualifying_popup_content,
+																$word_to_find,
+																$wrap_before,
+																$wrap_after
+															);
 	$qualifying_popup_content = str_replace('${category}', $category_title, $qualifying_popup_content );
 	$qualifying_popup_title   = get_field( 'qualifying_popup_title_override' );
 	$qualifying_popup_title   = $qualifying_popup_title ?
 															esc_attr( $qualifying_popup_title ) :
 															esc_attr( get_field( 'global_qualifying_popup_title', 'option' ) );
 	?>
-	<div class="modal fade xten-modal" id="contact-qualify-modal" tabindex="-1" role="dialog" aria-labelledby="Contact Form" aria-hidden="true">
+	<div class="modal fade xten-modal dynamicInput" id="contact-qualify-modal" tabindex="-1" role="dialog" aria-labelledby="Contact Form" aria-hidden="true">
 		<div class="modal-dialog modal-dialog-centered" role="document">
 			<div class="modal-content">
 				<header class="modal-header xten-theme-color-bg">
@@ -67,14 +75,20 @@ $rejection_popup_content = $rejection_popup_content ?
 													 wp_kses_post( $rejection_popup_content ) :
 													 wp_kses_post( get_field( 'global_rejection_popup_content', 'option', false ) );
 if ( $rejection_popup_content ) :
-	$rejection_popup_content = str_replace('${title}', $title, $rejection_popup_content );
+	// Find ${financial-institution} and wrap in span.
+	$qualifying_popup_content = xten_word_wrap( 
+																$qualifying_popup_content,
+																$word_to_find,
+																$wrap_before,
+																$wrap_after
+															);
 	$rejection_popup_content = str_replace('${category}', $category_title, $rejection_popup_content );
 	$rejection_popup_title   = get_field( 'rejection_popup_title_override' );
 	$rejection_popup_title   = $rejection_popup_title ?
 														 esc_attr( $rejection_popup_title ) :
 														 esc_attr( get_field( 'global_rejection_popup_title', 'option' ) );
 	?>
-	<div class="modal fade xten-modal" id="contact-rejection-modal" tabindex="-1" role="dialog" aria-labelledby="Contact Form" aria-hidden="true">
+	<div class="modal fade xten-modal dynamicInput" id="contact-rejection-modal" tabindex="-1" role="dialog" aria-labelledby="Contact Form" aria-hidden="true">
 		<div class="modal-dialog modal-dialog-centered" role="document">
 			<div class="modal-content">
 				<header class="modal-header xten-theme-color-bg">
