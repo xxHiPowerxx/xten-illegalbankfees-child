512985e4a27021d25e07dc59ea5d45e60cc3f889
Ricky Adams <ra@main.local>
2020-06-22 16:30:48 -0700

    correctly pass in size in xten_get_optimal_image_size()

--- template-parts/content-archive-post.php
+++ template-parts/content-archive-post.php
@@ -16,7 +16,7 @@
 			$image_id = get_post_thumbnail_id();
 			$wide_tall;
 			if ( $image_id ) :
-				$size = xten_get_optimal_image_size( $image_id, $size, array( 16, 9 ) );
+				$size = xten_get_optimal_image_size( $image_id, array( 450, null ), array( 16, 9 ) );
 				if ( is_array( $size ) ) :
 					$wide_tall = xten_wide_tall_image( $size );
 				else :
