c3abc5394733dce3fc0fc6fc45cec0b85b01be01
xxHiPowerxx <rickyadams432@gmail.com>
2021-04-26 12:20:51 -0700

    featured image width now varies based on sidebar_location theme_mod

--- template-parts/content-archive-post.php
+++ template-parts/content-archive-post.php
@@ -16,7 +16,12 @@
 			$image_id = get_post_thumbnail_id();
 			$wide_tall;
 			if ( $image_id ) :
-				$size = xten_get_optimal_image_size( $image_id, array( 450, null ), array( 16, 9 ) );
+				// Optimal Width of Image is larger when Sidebar is not present.
+				$sidebar_location = get_theme_mod( 'sidebar_location', 'sidebar_right' );
+				$image_width = 'sidebar_none' !== $sidebar_location ?
+					450 :
+					690;
+				$size = xten_get_optimal_image_size( $image_id, array( $image_width, null ), array( 16, 9 ) );
 				if ( is_array( $size ) ) :
 					$wide_tall = xten_wide_tall_image( $size );
 				else :
